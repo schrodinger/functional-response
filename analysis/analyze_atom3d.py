6822a233cf4884b09bcf2ae120dcc3adbd044346
Martin Voegele <martin.voegele@schrodinger.com>
2023-06-13 09:00:16 -0400

    input and results for atom3d poses (with wt binding pocket)

--- analysis/analyze_atom3d.py
+++ analysis/analyze_atom3d.py
@@ -7,11 +7,10 @@ import matplotlib.pyplot as plt
 os.makedirs('plots', exist_ok=True)
 
 print('\n * Original ATOM3D Poses *')
-dora1_table, adora1_perf = ana.analyze_study('adora1-atom3d', merge_by_name=True, 
+adora1_table, adora1_perf = ana.analyze_study('adora1-atom3d', merge_by_name=True, 
                                              combination='min', limits=[-22.5,7.5]
                                             )
 print('\n * MCS-Docked Poses *')
-adrb2_table, adrb2_perf = ana.analyze_study('adrb2', limits=[-27.5,2.5]) 
 adora1_table, adora1_perf = ana.analyze_study('adora1-atom3d-mcs', merge_by_name=True, 
                                               combination='min', limits=[-22.5,7.5]
                                              )
